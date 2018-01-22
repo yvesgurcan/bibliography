142c758db8c4e8a3a85937da7948b218f501e365
yvesgurcan <owner@AsteroideB612.domain>
2018-01-21 19:26:19 -0800

    server log and timestamp

--- server.js
+++ server.js
@@ -6,20 +6,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/references', (req, res) => {
+  console.log(Date() + " - get references")
   res.send({
-      references: [
-        {
-            name: "Reference1",
-            url: "test",
-        },
-      ],
+    references: [
+      {
+        name: "Reference1",
+        url: "test",
+      },
+    ],
   })
 })
 
-app.get('/signIn', (req, res) => {
-    res.send({
-        authenticated: true,
-    })
+app.post('/signIn', (req, res) => {
+  console.log(Date() + " - post sign in")
+  res.send({
+    authenticated: true,
   })
+})
 
-app.listen(port, () => console.log(`Hello! Test server is listening on port ${port}.`))
+app.listen(port, () => console.log(Date() + ` - Hello! Test server is listening on port ${port}.`))
